960d59f993edc3cf8f0a0a605fcde9e282849d39
JungInGyu <112679529+JungInGyu@users.noreply.github.com>
2024-06-03 17:45:59 +0900

    [level 0] Title: 분수의 덧셈, Time: 9.67 ms, Memory: 74 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {
+        int[] answer = new int[2];
+        int den = denom1;
+        int den2 = denom2;
+        int num1 = numer1;
+        int num2 = numer2;
+        int count = 2;
+        int countDe = 2;
+        if(den != den2){
+            while(den != den2){
+                if(den < den2){
+                    den = denom1;
+                    num1 = numer1;
+                    den *= count;
+                    num1 *= count;
+                    count++;
+                } else if(den > den2){
+                    den2 = denom2;
+                    den2*=countDe;
+                    num2 = numer2;
+                    num2 *= countDe;
+                    countDe++;
+                }
+            }
+        }
+        System.out.print(num1 + "\t" + num2 );
+        int gcd = gcd((num1+num2),den);
+        answer[0] = (num1 + num2) / gcd;
+        answer[1] = den/gcd;
+        return answer;
+    }
+    private int gcd(int x, int y) {
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+        return x;
+    }
+}
