f9cb3bcc7cf8e40e38324ffc6ab66c03d4d510b3
JungInGyu <112679529+JungInGyu@users.noreply.github.com>
2024-06-12 17:16:57 +0900

    [level 2] Title: 올바른 괄호, Time: 39.04 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                stack.push(ch);
+            }
+            if(ch == ')'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return answer = (stack.isEmpty()) ? true : false;
+    }
+}
